313f702aa3041cf75364562d139db877a7fd8e62
badmagick329 <uzairfarooqi@gmail.com>
2025-05-18 01:12:27 +0100

    feat: make page responsive. add dummy hamburger navbar

---
+++ src/scripts/convertimages.ps1
@@ -0,0 +1,8 @@
+ffmpeg -y -i ..\assets\Images\gallery1.jpg -vf scale=500:-1 ..\assets\Images\gallery1-thumbnail.jpg
+ffmpeg -y -i ..\assets\Images\gallery2.jpg -vf scale=500:-1 ..\assets\Images\gallery2-thumbnail.jpg
+ffmpeg -y -i ..\assets\Images\gallery3.jpg -vf scale=500:-1 ..\assets\Images\gallery3-thumbnail.jpg
+ffmpeg -y -i ..\assets\Images\gallery4.jpg -vf scale=500:-1 ..\assets\Images\gallery4-thumbnail.jpg
+ffmpeg -y -i ..\assets\Images\gallery5.jpg -vf scale=500:-1 ..\assets\Images\gallery5-thumbnail.jpg
+ffmpeg -y -i ..\assets\Images\gallery6.jpg -vf scale=500:-1 ..\assets\Images\gallery6-thumbnail.jpg
+ffmpeg -y -i ..\assets\Images\gallery7.jpg -vf scale=500:-1 ..\assets\Images\gallery7-thumbnail.jpg
+ffmpeg -y -i ..\assets\Images\gallery8.jpg -vf scale=500:-1 ..\assets\Images\gallery8-thumbnail.jpg
