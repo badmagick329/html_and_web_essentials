56434d7f2620a15a8d6e33e4a3cd34d4588c4374
badmagick329 <uzairfarooqi@gmail.com>
2025-05-19 20:05:48 +0100

    fix: page overflow

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -13,24 +13,4 @@ function initSplide() {
   }).mount();
 }
 
-function initBackToTopButton() {
-  const backToTopButton = document.querySelector('.back-to-top');
-
-  if (backToTopButton) {
-    window.addEventListener('scroll', () => {
-      if (window.scrollY > 300) {
-        backToTopButton.classList.add('visible');
-      } else {
-        backToTopButton.classList.remove('visible');
-      }
-    });
-    window.addEventListener('click', () => {
-      window.scrollTo({
-        top: 0,
-        behavior: 'smooth',
-      });
-    });
-  }
-}
-
 main();
