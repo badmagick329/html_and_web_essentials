f4bb2750a6a94592f2c00e824386d0bfa00c844e
badmagick329 <uzairfarooqi@gmail.com>
2025-05-19 19:34:35 +0100

    feat: add accessibility attributes and back to top button

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -1,4 +1,9 @@
 function main() {
+  initSplide();
+  initBackToTopButton();
+}
+
+function initSplide() {
   new Splide('#splide', {
     type: 'loop',
     perPage: 1,
@@ -6,6 +11,26 @@ function main() {
     arrows: true,
     pagination: true,
   }).mount();
+}
+
+function initBackToTopButton() {
+  const backToTopButton = document.querySelector('.back-to-top');
+
+  if (backToTopButton) {
+    window.addEventListener('scroll', () => {
+      if (window.scrollY > 300) {
+        backToTopButton.classList.add('visible');
+      } else {
+        backToTopButton.classList.remove('visible');
+      }
+    });
+    window.addEventListener('click', () => {
+      window.scrollTo({
+        top: 0,
+        behavior: 'smooth',
+      });
+    });
+  }
 }
 
 main();
